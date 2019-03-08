350caa00f2e5e1f89d0c009215359534ea1d42f5
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-03-08 11:04:10 -0500

    models updated

--- rainforest/rainforest/models.py
+++ rainforest/rainforest/models.py
@@ -5,8 +5,14 @@ class Product (models.Model):
     description = models.CharField(max_length = 255)
     price = models.IntField()
 
+    def __str__(self):
+        return self.name
+
 class Review (models.Model):
     name = models.CharField(max_length=255)
     date_created = models.DateField(auto_now_add=True)
     description = models.TextField()
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
+
+    def __str__(self):
+        return "{}, {}".format(self.name, self.date_created)
