268ea8b23e7c4cd8355a0a014fdc6aca4368169f
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-03-08 11:06:51 -0500

    models fixed, migrate

--- rainforest/rainforest/models.py
+++ rainforest/rainforest/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Product (models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length = 255)
-    price = models.IntField()
+    price = models.IntegerField()
 
     def __str__(self):
         return self.name
